33e45d9119a6bb740efcbcea1a2b988b9cbc90f2
MontenegroDiogo <diogomonteve@gmail.com>
2024-11-08 02:27:42 -0300

    firt commit

---
+++ script.js
@@ -0,0 +1,310 @@
+function Resposta1(){
+    let res = document.querySelector('#input1').value;
+
+    switch (res) {
+        case "A":
+            // console.log("Errado");
+            // alert("Errado");
+            document.querySelector('#resposta').innerHTML="Que isso Dev? Tente movamente.";
+            break
+            case "a":
+            // console.log("Errado");
+            // alert("Errado");
+            document.querySelector('#resposta').innerHTML="Que isso Dev? Tente movamente.";
+            break;
+        case "B":
+            // console.log("Correto");
+            // alert("Correto");
+            document.querySelector('#resposta').innerHTML="Correto";
+            break
+            case "b":
+            // console.log("Correto");
+            // alert("Correto");
+            document.querySelector('#resposta').innerHTML="Correto";
+            break;
+        case "C":
+            // console.log("Errado");
+            // alert("Errado");
+            document.querySelector('#resposta').innerHTML="Que isso Dev? Tente movamente.";
+            break
+            case "c":
+            // console.log("Errado");
+            // alert("Errado");
+            document.querySelector('#resposta').innerHTML="Que isso Dev? Tente movamente.";
+            break;
+        default:
+            // alert("Valor não aceito");
+            // console.log("Valor não aceito");
+            document.querySelector('#resposta').innerHTML="Valor não aceito";
+            break
+
+
+    }
+}
+
+function Resposta2(){
+    let res2 = document.querySelector('#input2').value;
+
+    switch (res2) {
+        case "A":
+            document.querySelector('#resposta2').innerHTML="Errado";
+            break
+            case "a":
+            document.querySelector('#resposta2').innerHTML="Errado";
+            break
+        case "B":
+            document.querySelector('#resposta2').innerHTML="Errado"
+            break
+            case "b":
+                document.querySelector('#resposta2').innerHTML="Errado"
+                break
+
+        case "C":
+            document.querySelector('#resposta2').innerHTML="Certo"
+            break
+            case "c":
+                document.querySelector('#resposta2').innerHTML="Certo"
+                break
+        
+        case "P":
+            document.querySelector('#resposta2').innerHTML = "kkkkkkk boa, realmente é a tag 'P'. Mas porfavor, escolhas as alternativas A, B ou C"
+            break
+            case "p":
+            document.querySelector("#resposta2").innerHTML = "kkkkkkk boa, realmente é a tag 'P'. Mas porfavor, escolhas as alternativas A, B ou C"
+            break
+        default:
+            document.querySelector('#resposta2').innerHTML="Valor não aceito"
+            break
+
+        
+
+
+    }
+}
+
+
+function Resposta3() {
+    let res3 = document.querySelector('#input3').value;
+
+    switch (res3) {
+        
+        case "A":
+            document.querySelector("#resposta3").innerHTML = "Dispense Dev, ta errado."
+            break
+            case "a":
+            document.querySelector('#resposta3').innerHTML = "Dispense Dev, ta errado."
+            break
+        case "B":
+            document.querySelector('#resposta3').innerHTML = "Boa!"
+            break
+            case "b":
+                document.querySelector('#resposta3').innerHTML = "Boa!"
+            break
+        case "C":
+            document.querySelector('#resposta3').innerHTML = "ERROOUU"
+            break
+        case "c":
+            document.querySelector('#resposta3').innerHTML = "ERROOOUUU"
+            break
+        default:
+            document.querySelector('#resposta3').innerHTML = "Escolha as alternativas A,B ou C"
+    }
+}
+
+
+
+function Resposta4() {
+    let res4 = document.querySelector("#input4").value;
+
+    switch (res4) {
+        case "A":
+            document.querySelector("resposta4").innerHTML = "Errou"
+            break
+        case "a":
+            document.querySelector("#resposta4").innerHTML = "Errou"
+            break
+        case "B":
+            document.querySelector("#resposta4").innerHTML = "Boa!"
+            break
+        case "b":
+            document.querySelector("#resposta4").innerHTML = "boa!"
+            break
+        case "C":
+            document.querySelector("resposta4").innerHTML = "Errou"
+            break
+        case "c":
+            document.querySelector('#resposta4').innerHTML = "errou"
+            break
+        default:
+            document.querySelector('#resposta4').innerHTML = "Escolha A, B ou C" 
+
+    }
+}
+
+function Resposta5() {
+    let res5 = document.querySelector("#input5").value;
+
+    switch (res5) {
+        case "A":
+            document.querySelector("resposta5").innerHTML = "Errou"
+            break
+        case "a":
+            document.querySelector("#resposta5").innerHTML = "Errou"
+            break
+        case "B":
+            document.querySelector("#resposta5").innerHTML = "Errou!"
+            break
+        case "b":
+            document.querySelector("#resposta5").innerHTML = "Errou!"
+            break
+        case "C":
+            document.querySelector("resposta5").innerHTML = "Boa"
+            break
+        case "c":
+            document.querySelector('#resposta5').innerHTML = "boa"
+            break
+        default:
+            document.querySelector('#resposta5').innerHTML = "Escolha A, B ou C" 
+
+    }
+}
+
+function Resposta6() {
+    let res6 = document.querySelector("#input6").value;
+
+    switch (res6) {
+        case "A":
+            document.querySelector("resposta6").innerHTML = "Errou"
+            break
+        case "a":
+            document.querySelector("#resposta6").innerHTML = "Errou"
+            break
+        case "B":
+            document.querySelector("#resposta6").innerHTML = "Boa!"
+            break
+        case "b":
+            document.querySelector("#resposta6").innerHTML = "boa!"
+            break
+        case "C":
+            document.querySelector("resposta6").innerHTML = "Errou"
+            break
+        case "c":
+            document.querySelector('#resposta6').innerHTML = "errou"
+            break
+        default:
+            document.querySelector('#resposta6').innerHTML = "Escolha A, B ou C" 
+
+    }
+}
+
+function Resposta7() {
+    let res7 = document.querySelector("#input7").value;
+
+    switch (res7) {
+        case "A":
+            document.querySelector("resposta7").innerHTML = "Errou"
+            break
+        case "a":
+            document.querySelector("#resposta7").innerHTML = "Errou"
+            break
+        case "B":
+            document.querySelector("#resposta7").innerHTML = "Boa!"
+            break
+        case "b":
+            document.querySelector("#resposta7").innerHTML = "boa!"
+            break
+        case "C":
+            document.querySelector("resposta7").innerHTML = "Errou"
+            break
+        case "c":
+            document.querySelector('#resposta7').innerHTML = "errou"
+            break
+        default:
+            document.querySelector('#resposta7').innerHTML = "Escolha A, B ou C" 
+
+    }
+}
+
+function Resposta8() {
+    let res8 = document.querySelector("#input8").value;
+
+    switch (res8) {
+        case "A":
+            document.querySelector("resposta8").innerHTML = "Boa!"
+            break
+        case "a":
+            document.querySelector("#resposta8").innerHTML = "boa!"
+            break
+        case "B":
+            document.querySelector("#resposta8").innerHTML = "Errou!"
+            break
+        case "b":
+            document.querySelector("#resposta8").innerHTML = "Errou!"
+            break
+        case "C":
+            document.querySelector("resposta8").innerHTML = "Errou"
+            break
+        case "c":
+            document.querySelector('#resposta8').innerHTML = "errou"
+            break
+        default:
+            document.querySelector('#resposta8').innerHTML = "Escolha A, B ou C" 
+
+    }
+}
+
+function Resposta9() {
+    let res9 = document.querySelector("#input9").value;
+
+    switch (res9) {
+        case "A":
+            document.querySelector("resposta9").innerHTML = "Errou"
+            break
+        case "a":
+            document.querySelector("#resposta9").innerHTML = "Errou"
+            break
+        case "B":
+            document.querySelector("#resposta9").innerHTML = "Errou!"
+            break
+        case "b":
+            document.querySelector("#resposta9").innerHTML = "Errou!"
+            break
+        case "C":
+            document.querySelector("resposta9").innerHTML = "Boa!"
+            break
+        case "c":
+            document.querySelector('#resposta9').innerHTML = "boa!"
+            break
+        default:
+            document.querySelector('#resposta9').innerHTML = "Escolha A, B ou C" 
+
+    }
+}
+
+function Resposta10() {
+    let res10 = document.querySelector("#input10").value;
+
+    switch (res10) {
+        case "A":
+            document.querySelector("resposta10").innerHTML = "Errou"
+            break
+        case "a":
+            document.querySelector("#resposta10").innerHTML = "Errou"
+            break
+        case "B":
+            document.querySelector("#resposta10").innerHTML = "Errou!"
+            break
+        case "b":
+            document.querySelector("#resposta10").innerHTML = "Errou!"
+            break
+        case "C":
+            document.querySelector("resposta10").innerHTML = "boa"
+            break
+        case "c":
+            document.querySelector('#resposta10').innerHTML = "boa"
+            break
+        default:
+            document.querySelector('#resposta10').innerHTML = "Escolha A, B ou C" 
+
+    }
+}
